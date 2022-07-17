cf916ba2585ad39f1730ef7541b368b1e167e5c8
ssamkkim <ssamuel.kkim@gmail.com>
2022-07-17 00:55:56 -0700

    Base File Structure + Basic Page Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      break: 5,
+      session: 25,
+      label: 'Session',
+      time: '25:00'
+    };
+  }
+  render(){
+    return (
+      <div id="app">
+        <div id="container">
+          <div id="length-container">
+            <div id="break-container">
+              <div id="break-label">Break Length</div>
+              <button id="break-decrement"></button>
+              <button id="break-increment"></button>
+              <div id="break-length">{this.state.break}</div>
+              </div>
+            <div id="session-container">
+              <div id="session-label">Session Length</div>
+              <button id="session-decrement"></button>
+              <button id="session-increment"></button>
+              <div id="session-length">{this.state.session}</div>
+            </div>
+          </div>
+          <div id="display">
+            <div id="timer-label">{this.state.label}</div>
+            <div id="time-left">{this.state.time}</div>
+          </div>
+          <div id="button-container">
+            <button id="start_stop"></button>
+            <button id="reset"></button>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
